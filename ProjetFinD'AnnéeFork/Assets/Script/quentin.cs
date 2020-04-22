2464587bfeedcdfa287e32f4a0c80ecbf318da3e
QuentinSinyZ <quentin.lunal@outlook.fr>
2020-04-22 15:27:44 +0200

    new script+ modification scriptquentin

--- ProjetFinD'AnnéeFork/Assets/Script/quentin.cs
+++ ProjetFinD'AnnéeFork/Assets/Script/quentin.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class quentin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+   là normalement ça marche
     }
 }
